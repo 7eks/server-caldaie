ec38b3bd32ecd103e371a9c05e7acb7beb750d07
7eks <7eks@protonmail.com>
2017-04-26 15:01:31 +0200

    rimosso svg e sostituito con testo che verra rimpiazzato dal widget

--- public/js/client.js
+++ public/js/client.js
@@ -2,8 +2,7 @@
 const ar = Array.from(document.querySelectorAll('*[id]'))
 console.log(ar)
 
-const cerchio=['<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><defs><filter height="200" width="200" y="-50" x="-50" id="svg_1_blur"><feGaussianBlur stdDeviation="10" in="SourceGraphic"/></filter></defs><title>  background</title><rect id="canvas_background" height="100" width="100" y="-1" x="-1" fill="#fff"/><g height="100" width="100" id="canvasGrid"><rect height="100" width="100" fill="url(#gridpattern)"/></g><title>  Layer 1</title><ellipse filter="url(#svg_1_blur)" ry="5" rx="5" id="svg_1" cy="50" cx="50" style="fill:#CE7975;stroke-width:2;stroke:#000"/></svg>',
-'<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><defs><filter height="200" width="200" y="-50" x="-50" id="svg_1_blur"><feGaussianBlur stdDeviation="10" in="SourceGraphic"/></filter></defs><title>  background</title><rect id="canvas_background" height="100" width="100" y="-1" x="-1" fill="#fff"/><g height="100" width="100" id="canvasGrid"><rect height="100" width="100" fill="url(#gridpattern)"/></g><title>  Layer 1</title><ellipse filter="url(#svg_1_blur)" ry="5" rx="5" id="svg_1" cy="50" cx="50" style="fill:#A0D58A;stroke-width:2;stroke:#000"/></svg>']
+const cerchio=[' led rosso','  led verde']
 
 const dati = [{
   name: 'prs_caldaia2',
@@ -24,6 +23,6 @@ for (let x in dati) {
   if (b.length > 0){
     var c = ar.filter(check_presence(b[0].id + '_widget'))
     console.log(c);
-    (c[0].attributes[2].value == 'bolean')?c[0].innerHTML = cerchio[dati[x].value]:c[0].innerHTML = dati[x].value
+    (c[0].attributes[2].value == 'bolean')?c[0].innerHTML = cerchio[dati[x].value]:c[0].innerHTML = '  ' + dati[x].value
   }
 }
